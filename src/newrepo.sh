b325d5c7d7dd492fab2ee8fa7436c5a7c3358183
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2021-03-02 21:57:13 -0300

    chore(params): finish my newrepo config and add required and optional arguments

--- src/newrepo.sh
+++ src/newrepo.sh
@@ -1,43 +1,57 @@
 #!/bin/bash
 
 newRepo () {
-  # Create folders
-  echo "Creating folders..."
-  mkdir $1
-  cd $1
-  mkdir src
-  mkdir node_modules
-  echo "Folders ready"
+  # Comprobate first argument
+  if [ ! -z $1 ]
+  then
+    # Create folders
+    echo "Creating folders..."
+    mkdir $1
+    cd $1
+    mkdir src
+    mkdir node_modules
+    echo "Folders ready"
 
-  # Copy & paste files
-  echo "Copying & pasting files..."
-  cp -rT ~/repo/new-repo/src/files ./
-  echo "Files ready"
+    # Copy & paste files
+    echo "Copying & pasting files..."
+    cp -rT ~/repo/newrepo/src/files ./
+    echo "Files ready"
 
-  # Node modules install
-  echo "Installing node modules..."
-  npm init -y
-  npm install --save-dev standard-version @commitlint/{config-conventional,cli} husky
-  node -e "let pkg=require('./package.json'); pkg.scripts.release='standard-version'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));"
-  echo "Node modules ready"
+    # Node modules install
+    echo "Installing node modules..."
+    npm init -y
+    npm install --save-dev standard-version @commitlint/{config-conventional,cli} husky
+    node -e "let pkg=require('./package.json'); pkg.scripts.release='standard-version'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));"
+    echo "Node modules ready"
 
-  # Git start
-  echo "Starting git repo..."
-  git init
+    # Git start
+    echo "Starting git repo..."
+    git init
 
-  ## Hooks
-  npx husky install
-  echo "module.exports = { extends: ['@commitlint/config-conventional'] }" > commitlint.config.js
-  npx husky add .husky/commit-msg "npx --no-install commitlint --edit \$1"
+    ## Hooks
+    npx husky install
+    echo "module.exports = { extends: ['@commitlint/config-conventional'] }" > commitlint.config.js
+    npx husky add .husky/commit-msg "npx --no-install commitlint --edit \$1"
 
-  git add -A
-  git commit -m "build(repo): start"
-  git branch -m master main
-  git branch test
-  git remote add origin $2
-  git push origin main
-  echo "Git ready"
-  echo "New repo completed"
+    git add -A
+    git commit -m "chore(repo): start"
+    git branch -m master main
+    git branch test
+
+    ## Connect to remote repositorie
+    if [ ! -z $2 ]
+    then
+      git remote add origin $2
+      git push origin main
+    fi
+
+    echo "Git ready"
+    echo "New repo completed"
+
+  else
+    echo "Project name required"
+
+  fi
 }
 
 newRepo $1 $2
