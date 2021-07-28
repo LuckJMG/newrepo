ff7b35394875ada9a7670f41efb3f0b51d82d03f
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2021-07-28 12:36:03 -0400

    feat(debug): add error codes

--- src/main.sh
+++ src/main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #* New repository creation
-# Declare current lib path
+# Declare constants
 LIB_PATH="$(dirname "$0")/lib"
+OPTIONS=("--minimalist" "--standard" "--node" "--python" "--web")
 
-echo "Creating new repository..."
+function newrepo() {
+  echo "Creating new repository..."
 
-# Check first parameter
-if [ -n "$1" ]; then
   # Create required folders and files
   "$LIB_PATH"/folder.sh "$1" "$2"
   "$LIB_PATH"/file.sh "$1" "$2"
@@ -21,6 +21,41 @@ if [ -n "$1" ]; then
   "$LIB_PATH"/git.sh "$1" "$2" "$3"
 
   echo "New repository created"
+}
+
+# Check first parameter
+if [ -n "$1" ]; then
+
+  # Check if if first parameter is an option
+  if [[ "$1" = -* ]]; then
+
+    # Check if that option exist
+    for option in "${OPTIONS[@]}"; do
+      if [ "$1" == "$option" ]; then
+        option_exist=true
+      fi
+    done
+
+    if [ $option_exist ]; then
+
+      # If exist check second parameter
+      if [ -n "$2" ]; then
+        newrepo "$1" "$2" "$3"
+
+      else # If second parameter is missing
+        echo "ERROR[1.0.2]: MISSING_PARAMETER"
+        echo "The second parameter is missing"
+      fi
+
+    else # If the option not exist
+      echo "ERROR[2.0.1]: WRONG_OPTION"
+      echo "The option $1 doesn't exist"
+    fi
+
+  else # If not an option execute default
+    newrepo "$1" "$2"
+  fi
+
 else # If first parameter is missing
   echo "ERROR[1.0.1]: MISSING_PARAMETER"
   echo "The first parameter is missing"
